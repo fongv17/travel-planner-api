335a5abed232c7ef578bbba246bd9f48a88d68b0
Brandon Shy <bshy201@gmail.com>
2025-11-21 18:14:15 -0500

    fixed schemas

--- prisma/seed.js
+++ prisma/seed.js
@@ -5,21 +5,29 @@ try {
   await prisma.user.deleteMany();
 
   const usersData = [
-    {
-      email: 'Brandon@demo.com',
-      password: await bcrypt.hash('brandon1234', 10),
-      role: 'ADMIN',
-    },
+    
     {
       email: 'Trevor@demo.com',
       password: await bcrypt.hash('Trevor456', 10),
     },
   ];
 
-  for (const user of usersData ){
-    prisma.user.create({ data: user }),
-  };
+  const users = await Promise.all(
+    usersData.map((user) => prisma.user.create({ data: user })),
+  );
 
+ for (const user of users) {
+    await prisma.post.createMany({
+      data: [
+        {
+         name: "Tokyo Trip",
+         startDate: "06-07-2026",
+          endDate: "06-10-2026",
+          userId: user.id,
+        },
+      ],
+    });
+  }
   
   console.log('Seed completed successfully!');
 } catch (error) {
