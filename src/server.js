85f7c19a99cdc4fd495e9ff12fd1104bcf3f6a42
Fong Vang <118277142+fongv17@users.noreply.github.com>
2025-11-24 06:52:50 -0500

    added root route

--- src/server.js
+++ src/server.js
@@ -17,7 +17,9 @@ app.use(morgan('tiny'));
 
 const specs = YAML.load('./public/bundled.yaml')
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(specs))
-
+app.get('/', (req, res) => {
+  res.status(200).json({ message: 'api running' });
+});
 app.get('/health', (req, res) => {
   res.status(200).json({ status: 'ok' });
 });
