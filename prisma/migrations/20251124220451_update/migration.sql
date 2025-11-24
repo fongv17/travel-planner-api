9104cd4e69a4eea92855f2492efd64a6f197bbf2
jacob-sasser <jacobsasser23@gmail.com>
2025-11-24 17:05:05 -0500

    update

---
+++ prisma/migrations/20251124220451_update/migration.sql
@@ -0,0 +1,14 @@
+/*
+  Warnings:
+
+  - Added the required column `name` to the `users` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- DropForeignKey
+ALTER TABLE "trip" DROP CONSTRAINT "trip_userId_fkey";
+
+-- AlterTable
+ALTER TABLE "users" ADD COLUMN     "name" TEXT NOT NULL;
+
+-- AddForeignKey
+ALTER TABLE "trip" ADD CONSTRAINT "trip_userId_fkey" FOREIGN KEY ("userId") REFERENCES "users"("id") ON DELETE CASCADE ON UPDATE CASCADE;
